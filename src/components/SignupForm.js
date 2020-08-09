7e3c364d239663ce5c95d4b32279a32e830d546b
Clayton Jones <claytonjonesLMP@gmail.com>
2020-08-08 17:40:53 -0700

    removing unused vars, started rtk

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+// import React from 'react';
+// import TextField from '@material-ui/core/TextField';
 
-import useForm from '../hooks/useForm'
+// import useForm from '../hooks/useForm'
 
-function SignupForm(props) {
-  const fields = {
-    username: '',
-    password: '',
-    confirmed: '',
-  };
+// function SignupForm(props) {
+//   const fields = {
+//     username: '',
+//     password: '',
+//     confirmed: '',
+//   };
 
-  const { handleChange, handleSubmit } = useForm(fields);
+//   // const { handleChange, handleSubmit } = useForm(fields);
 
-  return (
-    <form className='signup-form' autoComplete="off">
-      <TextField id="username" label="Username" />
-      <TextField id="password" label="Password" />
-      <TextField id="confirmed" label="Confirm Password" />
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-    </form>
-  );
-}
+//   return (
+//     <form className='signup-form' autoComplete="off">
+//       <TextField id="username" label="Username" />
+//       <TextField id="password" label="Password" />
+//       <TextField id="confirmed" label="Confirm Password" />
+//       <Button variant="contained" color="primary">
+//         Primary
+//       </Button>
+//     </form>
+//   );
+// }
 
-export default SignupForm;
+// export default SignupForm;
