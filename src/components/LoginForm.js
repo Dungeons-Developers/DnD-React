c8719b31c92df071200e1538321b2f7118ccd836
Joel Watson <joel.watson92@gmail.com>
2020-08-10 21:50:29 -0700

    Finished layout on login/signup forms

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 import useForm from '../hooks/useForm'
 
-import {login} from '../store/slices/user-slice';
+import { login } from '../store/slices/user-slice';
 
-function LoginForm({login}) {
+function LoginForm({ login }) {
   const fields = {
     username: '',
     password: '',
@@ -28,15 +29,21 @@ function LoginForm({login}) {
 
   return (
     <form className='login-form' noValidate autoComplete="off" onSubmit={submit}>
-      <TextField id="username" label="Username" onChange={formChange}/>
-      <TextField id="password" label="Password" onChange={formChange} type="password" />
-      <Button variant="contained" color="primary" type='submit'>
-        Log In
-      </Button>
-    </form>
+      <div>
+        <TextField id="username" label="Username" fullWidth onChange={formChange} />
+      </div>
+      <div>
+        <TextField id="password" label="Password" fullWidth onChange={formChange} type="password" />
+      </div>
+      <Box my="1rem">
+        <Button variant="contained" color="primary" fullWidth type='submit'>
+          Log In
+        </Button>
+      </Box>
+    </form >
   );
 }
 
-const mapDispatchToProps = {login};
+const mapDispatchToProps = { login };
 
 export default connect(null, mapDispatchToProps)(LoginForm);
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
-
+import Box from '@material-ui/core/Box'
 import useForm from '../hooks/useForm'
+
+import { create } from '../store/slices/user-slice';
 
 function SignupForm(props) {
   const fields = {
@@ -13,16 +16,36 @@ function SignupForm(props) {
 
   const { handleChange, handleSubmit } = useForm(fields);
 
+  function submit(e) {
+    e.preventDefault();
+    handleSubmit(create);
+    e.target.reset();
+  }
+
+  function formChange(e) {
+    handleChange(e.target.id, e.target.value);
+  }
+
   return (
-    <form className='signup-form' autoComplete="off">
-      <TextField id="username" label="Username" />
-      <TextField id="password" label="Password" />
-      <TextField id="confirmed" label="Confirm Password" />
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
+    <form className='login-form' noValidate autoComplete="off" onSubmit={submit}>
+      <div>
+        <TextField id="username" label="Username" fullWidth onChange={formChange} />
+      </div>
+      <div>
+        <TextField id="password" label="Password" fullWidth onChange={formChange} type='password' />
+      </div>
+      <div>
+        <TextField id="confirmed" label="Confirm Password" fullWidth onChange={formChange} type='password' />
+      </div>
+      <Box my="1rem">
+        <Button variant="contained" fullWidth color="primary" type='submit'>
+          Signup
+        </Button>
+      </Box>
     </form>
   );
 }
 
-export default SignupForm;
+const mapDispatchToProps = { create };
+
+export default connect(null, mapDispatchToProps)(SignupForm);
