815b88442a37decbb89890ed49269d169bccd500
Clayton Jones <claytonjonesLMP@gmail.com>
2020-08-11 21:43:07 -0700

    added header to routes, and created footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,7 @@ import {Link} from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
-  Typography,
   Tooltip,
-  IconButton
 } from '@material-ui/core';
 
 import Logo from '../Logo';
@@ -27,9 +25,8 @@ export default function Header(props) {
     <AppBar position='fixed' style={styles.appbar}>
       <Toolbar style={styles.toolbar}>
         <Tooltip title='Home'>
-          <Link to='/'>
+          <Link to='/home'>
             <Logo />
-            {/* <h1>Logo</h1> */}
           </Link>
         </Tooltip>
         <Nav />
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Logo(props) {
 
   return (
-    <img src="/assets/lightmode.svg" alt="logo"/>
+    <img src="/assets/lightmode.svg" alt="logo" height={props.height ? props.height : 100}/>
   )
 }
