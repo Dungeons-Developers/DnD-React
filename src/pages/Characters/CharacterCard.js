eefb1bfc022a8d8a744e0012eb47d459ce2d3796
dannevernguyens <danguyenuw@gmail.com>
2020-08-11 18:50:13 -0700

    fixed modal layer issue

--- src/pages/Characters/CharacterCard.js
+++ src/pages/Characters/CharacterCard.js
@@ -62,7 +62,7 @@ export default function CharacterCard() {
   };
 
   const body = (
-    <div style={modalStyle} className={classes.paper}>
+    <div style={modalStyle} className={modalClasses.paper}>
       <h2 id="simple-modal-title">Text in a modal</h2>
       <p id="simple-modal-description">
         Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
